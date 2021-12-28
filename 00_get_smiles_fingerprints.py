373be87ec8fe720552e3c0f447e48e7010d42a4a
bii-dpi <>
2021-12-28 12:55:55 +0800

    Added ligand fingerprint

---
+++ 00_get_smiles_fingerprints.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from progressbar import progressbar
+from concurrent.futures import ProcessPoolExecutor
+from rdkit.Chem import DataStructs
+
+
+def get_fingerprint(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024)
+    arr = np.zeros((0, ), dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    return arr
+
+
+def get_smiles(path):
+    with open(path, "r") as f:
+        return set([line.split()[0] for line in f.readlines()])
+
+
+def write_featurized(dataset, suffix):
+    with open(f"../get_data/{dataset}/{dataset.lower()}_{suffix}", "r") as f:
+        smiles = [line.split()[0] for line in f.readlines()]
+
+    np.save(f"data/{dataset.lower()}_{suffix}_smiles_tokens.npy",
+            FEATURIZER.featurize(smiles,
+                                 log_every_n=1e5))
+
+
+all_smiles = set()
+for dataset in progressbar(["BindingDB", "DUDE"]):
+    for suffix in ["actives", "decoys"]:
+        all_smiles |= \
+            get_smiles(f"../get_data/{dataset}/{dataset.lower()}_zs_{suffix}")
+
+all_fingerprints = []
+for smiles in progressbar(all_smiles):
+    all_fingerprints.append(get_fingerprint(smiles))
+
+print(all_fingerprints[0])
+
+all_fingerprints = np.vstack(all_fingerprints)
+np.save("data/all_fingerprints.npy", all_fingerprints)
+print(all_fingerprints.dtype)
+print(all_fingerprints.shape)
+
